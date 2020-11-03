4c31451b0c213bae42c21db7658c286f75f7e83b
lounge76 <Kilen123!>
2020-11-03 13:09:34 +0100

    correction

--- src/test/java/CSVRead.java
+++ src/test/java/CSVRead.java
@@ -33,7 +33,7 @@ public class CSVRead {
 
     @Test
      public void Syns_texten() throws IOException {
-        if (driver.findElement(By.xpath("//a[contains(.,'Only Testing!!!')]")).isDisplayed()) {
+        if (driver.findElement(By.xpath("//a[contains(.,'Only Testing')]")).isDisplayed()) {
             System.out.println("Element is Visible");
         } else {
             System.out.println("Element is InVisible");
