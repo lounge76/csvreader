649d13767a57eb5fda9b08626723167e4ed93b72
lounge76 <Kilen123!>
2020-11-03 12:53:30 +0100

    screenshot added

--- src/test/java/CSVRead.java
+++ src/test/java/CSVRead.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -23,11 +27,13 @@ public class CSVRead {
         driver.manage().deleteAllCookies();
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://only-testing-blog.blogspot.com/2014/05/form.html");
+
+
     }
 
     @Test
     public void forstaTesten() {
-        if (driver.findElement(By.xpath("//a[contains(.,'Only Testing!!')]")).isDisplayed()) {
+        if (driver.findElement(By.xpath("//a[contains(.,'Only Testing')]")).isDisplayed()) {
             System.out.println("Element is Visible");
         } else {
             System.out.println("Element is InVisible");
@@ -55,6 +61,13 @@ public class CSVRead {
             driver.findElement(By.xpath("//input[@value='Submit']")).click();
             driver.switchTo().alert().accept();
         }
+    }
+
+    @Test
+    public void Screenshot() throws IOException {
+        Screenshot screenshot = new AShot().takeScreenshot(driver);
+        ImageIO.write(screenshot.getImage(), "jpg", new File(".\\Screenshots\\screenshot_"+System.currentTimeMillis()+".jpg"));
+
     }
 
 
